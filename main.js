dbfa88f09d9571b070286fe3694f29db87296efa
Gokul <gokulsrinivas98@gmail.com>
2020-07-28 13:50:49 +0530

    prefix changes

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 const client = new Discord. Client();
-const prefix = '-';
+//const prefix = '-';
 //const fs =require('fs');
 //client.commands = new Discord.Collection();
 //const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
@@ -15,14 +15,16 @@ client.once('ready', () => {
 });
 
 client.on('message', message =>{
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-    
+    //if (!message.content.startsWith(prefix) || message.author.bot) return;
+    //const args = message.content.slice(prefix.length).split(/ +/);
+    //const command = args.shift().toLowerCase();
+    let prefix='-';
+    let command = messageArray[0];
+    let args = MessageArray.slice[0];
     let msga = message.author;
     let msgai=message.author.id;
     let SellRoleObject = message.guild.roles.cache.find(r=> r.name === 'Sell');
-    if(command === 'sellon'){
+    if(command === `${prefix}sellon`){
         message.channel.setName(`🟢┋sell-lobby`)
             .catch(console.error);
         
@@ -30,7 +32,7 @@ client.on('message', message =>{
         message.delete();
         
     }
-    else if (command === 'selloff'){
+    else if (command === `${prefix}selloff`){
         message.channel.setName(`🔴┋sell-lobby`)
             .catch(console.error);
         async function clear() {
@@ -43,11 +45,11 @@ client.on('message', message =>{
         clear();
             
         
-    }else if(command === 'ping'){
+    }else if(command === `${prefix}ping`){
         message.channel.send('pong!')
         message.channel.send(`${msga} is playing ping pong ${SellRoleObject}`);
         message.delete();
-    }else if (command === 'help'){
+    }else if (command === `${prefix}help`){
         
         const exampleEmbed = new Discord.MessageEmbed()
 	        .setColor('#0099ff')
